a8c35fbe726d8b59d4341173b7f9a1582fcc34e6
HuTest <114138761+Anwarti@users.noreply.github.com>
2023-05-24 12:09:07 +0200

    Shelf class gemaakt

---
+++ warehouse/src/include/Shelf.hpp
@@ -0,0 +1,16 @@
+#include"IContainer.hpp"
+#include"Pallet.hpp"
+#include<array>
+
+
+class Shelf:public IContainer
+{
+    private:
+        std::array<Pallet, 4>pallets;
+    public:
+        Shelf();
+        bool swapPallet(int slot1, int slot2);
+        bool isEmpty() override;
+        bool isFull() override;
+
+};
