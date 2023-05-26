256348377aef9d645eb86cfe685a2447524b5ccb
HuTest <114138761+Anwarti@users.noreply.github.com>
2023-05-26 23:27:25 +0200

    alles public gemaakt

--- warehouse/src/include/WareHouse.hpp
+++ warehouse/src/include/WareHouse.hpp
@@ -1,18 +1,28 @@
-#include "include/Shelf.hpp"
-#include "include/Employee.hpp"
+#ifndef WAREHOUSE_HPP
+#define WAREHOUSE_HPP
+
+#include "Pallet.hpp"
+#include "Shelf.hpp"
+#include "Employee.hpp"
 #include <vector>
+#include <array>
 
 
 class Warehouse {
-    private:
-        std::vector<Employee> employees;
-        std::vector<Shelf> shelves;
 
     public:
+       
+       
+        std::vector<Employee> employees;
+        std::vector<Shelf> shelves;
         Warehouse();
-
-        void addEmployee(Employee& employee);
-        void addShelf(Shelf& shelf);
+        void addEmployee(const Employee& employee);
+        void addShelf(const Shelf& shelf);
         bool rearrangeShelf(Shelf& shelf);
         bool pickItems(const std::string& itemName, int itemCount);
+        // bool pickItems(const std::string& itemName, int itemCount);
+
+
+
 };
+#endif
